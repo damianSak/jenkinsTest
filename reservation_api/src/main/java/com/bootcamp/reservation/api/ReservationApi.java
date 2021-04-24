9ab3c3b0c02a6b6ee62d6c3254e1b23624951693
damianSak <damian.sak88@gmail.com>
2021-04-24 23:03:04 +0200

    initial

---
+++ reservation_api/src/main/java/com/bootcamp/reservation/api/ReservationApi.java
@@ -0,0 +1,39 @@
+package com.bootcamp.reservation.api;
+
+import com.bootcamp.reservation.repository.ReservationRepository;
+import com.bootcamp.reservation.model.Reservation;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@EnableSwagger2
+@RequestMapping(path = "/reservation")
+public class ReservationApi {
+    //TODO: wstrzyknąć sobie jakoś Repository
+
+    private final ReservationRepository reservationRepository;
+
+    public ReservationApi(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    @PostMapping
+    public void saveReservation(@RequestBody Reservation reservation) {
+        reservationRepository.createReservation(reservation);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Reservation> getReservation(@PathVariable("id") UUID id) {
+        return reservationRepository.findReservation(id);
+    }
+
+    @GetMapping
+    public List<Reservation> getAllReservations(){
+        return reservationRepository.findAllReservations();
+    }
+    //TODO: metody http GET po UUID, GET dla wszystkich, POST
+}
---
+++ reservation_api/src/main/java/com/bootcamp/reservation/model/Reservation.java
@@ -0,0 +1,22 @@
+package com.bootcamp.reservation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reservation {
+    UUID uuid;
+    UUID flightId;
+    LocalDateTime reservationDateTime;
+    TicketClass ticketClass;
+    BigDecimal price;
+    Passenger passenger;
+
+}
